1c50ea4865ce6d1c3535e0dcde4750d922684856
evelynbtr <57357507+evelynbtr@users.noreply.github.com>
2019-11-20 12:35:21 +0100

    Update tabellen_erstellen.sql
    unkorrigierte version beschreibungen titel etc

--- tabellen_erstellen.sql
+++ tabellen_erstellen.sql
@@ -31,49 +31,49 @@ VALUES("Verrückte Hunde Lady Sticker", "An alle Crazy Dog Ladys hier ist der pe
 INSERT INTO produkt(name, beschreibung, bild_datei, preis)
 VALUES("Bienen Sticker", "Cooler Bienen Sticker als Geschenkidee", "QUEEN BEE.png", 4.99);
 INSERT INTO produkt(name, beschreibung, bild_datei, preis)
-VALUES("", "", "SIGN-SHARK.png", 3.99);
+VALUES("Hai Haie Taucher", "Lustiges Taucher Design zu Weihnachten oder Geburtstag", "SIGN-SHARK.png", 3.99);
 INSERT INTO produkt(name, beschreibung, bild_datei, preis)
-VALUES("", "", "doght indek w.png", 4.99);
+VALUES("Hunde Hund Geschenk", "Cooles Hunde Hundehalter Geschenk zur Weihnachtszeit", "doght indek w.png", 4.99);
 INSERT INTO produkt(name, beschreibung, bild_datei, preis)
-VALUES("", "", "dogmom hund ejje.png", 2.99);
+VALUES("Hunde Sticker", "Dieser Hunde Sticker ist perfekt für Hundeliebhaber und Hunde Fans", "dogmom hund ejje.png", 2.99);
 INSERT INTO produkt(name, beschreibung, bild_datei, preis)
-VALUES("", "", "fische nagelrn.png", 4.99);
+VALUES("Angeln Fische", "Cooles Angler Geschenk Design zum Geburtstag für Angler", "fische nagelrn.png", 4.99);
 INSERT INTO produkt(name, beschreibung, bild_datei, preis)
-VALUES("", "", "girl who loves horses spruch.png", 3.99);
+VALUES("Pferde Spruch Sticker", "Dieses Coole Pferde Motiv ist perfekt für Reiterinen und Reiter", "girl who loves horses spruch.png", 3.99);
 INSERT INTO produkt(name, beschreibung, bild_datei, preis)
-VALUES("", "", "hell bee whiser.png", 2.99);
+VALUES("Biene Bienen Imker Sticker", "Hier ein cooler Imker Bienen Biene Sticker für Honig Liebhaber", "hell bee whiser.png", 2.99);
 INSERT INTO produkt(name, beschreibung, bild_datei, preis)
-VALUES("", "", "hell bride to bee 423.png", 3.99);
+VALUES("Bienen Braut Hochzeit", "Ein Cooler Braut Bienen Imker Sticker. Perfekt für Frisch Verheiratete", "hell bride to bee 423.png", 3.99);
 INSERT INTO produkt(name, beschreibung, bild_datei, preis)
-VALUES("", "", "helll pferd horse horse.png", 3.99);
+VALUES("Pferde Pferd Spruch Lustig", "Pferde sind doch des Menschen beste Freunde. Für alle Tierliebhaber", "helll pferd horse horse.png", 3.99);
 INSERT INTO produkt(name, beschreibung, bild_datei, preis)
-VALUES("", "", "hund spie hallwe.png", 1.99);
+VALUES("Hunde Halloween Hund Sticker", "Hier ein süßer Halloween Sticker für alle Hundeliebhaber", "hund spie hallwe.png", 1.99);
 INSERT INTO produkt(name, beschreibung, bild_datei, preis)
-VALUES("", "", "kinde einschuldun.png", 3.99);
+VALUES("Kinder Einschulung Geschenkidee", "Als Geschenkidee zur Einschulung ein Cooler Sticker für den Rucksack", "kinde einschuldun.png", 3.99);
 INSERT INTO produkt(name, beschreibung, bild_datei, preis)
-VALUES("", "", "kühehrkphebrk4.png", 3.99);
+VALUES("Kühe Farmer Kuh", "An alle Farmer, Bauer und Kuhliebhaber! Hier ist ein richtig cooler Sticker für euch!", "kühehrkphebrk4.png", 3.99);
 INSERT INTO produkt(name, beschreibung, bild_datei, preis)
-VALUES("", "", "kürbis katze3.png", 3.99);
+VALUES("Katze Halloween Kürbis", "Hier sehen sie das perfekte Halloween Geschenk für Katzenliebhaber", "kürbis katze3.png", 3.99);
 INSERT INTO produkt(name, beschreibung, bild_datei, preis)
-VALUES("", "", "nope cat.png", 3.99);
+VALUES("Nope Katze", "Für Faulenzer und Katzen Liebhaber. Hier ein Sticker der zum Ausdruck bringt was die Katze denkt", "nope cat.png", 3.99);
 INSERT INTO produkt(name, beschreibung, bild_datei, preis)
-VALUES("", "", "ofen wasser tauch.png", 3.99);
+VALUES("Taucher Tauchen Sticker", "Ein cooler lustiger Sticker für Taucher und Meer Liebhaber", "ofen wasser tauch.png", 3.99);
 INSERT INTO produkt(name, beschreibung, bild_datei, preis)
-VALUES("", "", "pferd 6.png", 3.99);
+VALUES("Pferde Pferd Sticker", "Mach deiner Freundin oder deiner Tochter mit diesem Kleinen Sticker eine Riesen Freude.", "pferd 6.png", 3.99);
 INSERT INTO produkt(name, beschreibung, bild_datei, preis)
-VALUES("", "", "pferde design 44.png", 3.99);
+VALUES("Pferde Design Pferd", "Hier ein cooler Pferde Sticker für Reiter und Mädchen die Prinzessin spielen.", "pferde design 44.png", 3.99);
 INSERT INTO produkt(name, beschreibung, bild_datei, preis)
-VALUES("", "", "pferde jkkliebe.png", 3.99);
+VALUES("Pferde Liebe Geschenk", "Pferde sind die besten Freunde des Menschen. Zeige deine Pferde Liebe mit diesem Sticker.", "pferde jkkliebe.png", 3.99);
 INSERT INTO produkt(name, beschreibung, bild_datei, preis)
-VALUES("", "", "pferde pferd.png", 3.99);
+VALUES("Pferde Pferd Sticker", "Pferde Sticker werden niemals aus dem Trend fallen, diese Tiere sind einfach fantastisch oder?", "pferde pferd.png", 3.99);
 INSERT INTO produkt(name, beschreibung, bild_datei, preis)
-VALUES("", "", "pferded 42.png", 3.99);
+VALUES("Pferde Pferd", "Cooles Pferde Geschenk Motiv als Sticker für Freunde und Familie", "pferded 42.png", 3.99);
 INSERT INTO produkt(name, beschreibung, bild_datei, preis)
-VALUES("", "", "reiten pferd 84939.png", 3.99);
+VALUES("Reiten Pferde Reiter", "Ein Cooler Pferde Sticker als Geschenkidee für Weihnachten oder Geburtstag", "reiten pferd 84939.png", 3.99);
 INSERT INTO produkt(name, beschreibung, bild_datei, preis)
-VALUES("", "", "save bees s.png", 3.99);
+VALUES("Save the Bees Bienen", "Rettet die Bienen oder tragt zumindest diesen Coolen Sticker um es auf die anderen zu schieben!", "save bees s.png", 3.99);
 INSERT INTO produkt(name, beschreibung, bild_datei, preis)
-VALUES("", "", "walking looser pfer d3.png", 3.99);
+VALUES("Pferde Lustiger Spruch", "Lustiger Pferde Spruch als Geschenkidee zu Weihnachten oder Geburtstag", "walking looser pfer d3.png", 3.99);
 
 
 
