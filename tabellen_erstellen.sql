d2d9bb54e9adf64fb0f62b103c616a44ea894945
evelynbtr <57357507+evelynbtr@users.noreply.github.com>
2019-11-20 12:17:47 +0100

    Update tabellen_erstellen.sql

--- tabellen_erstellen.sql
+++ tabellen_erstellen.sql
@@ -28,6 +28,55 @@ INSERT INTO produkt(name, beschreibung, bild_datei, preis)
 VALUES("Kaffe Hunde Hund Geschenk", "Für alle die Kaffee und Hunde lieben. Zum Geburtstag oder Weihnachten ", "coffe pet dogd eiwhund 4ik3mo.png", 2,99);
 INSERT INTO produkt(name, beschreibung, bild_datei, preis)
 VALUES("Verrückte Hunde Lady Sticker", "An alle Crazy Dog Ladys hier ist der perfekte Sticker. ", "dog lady hunde.png", 4,99);
+INSERT INTO produkt(name, beschreibung, bild_datei, preis)
+VALUES("Bienen Sticker", "Cooler Bienen Sticker als Geschenkidee", "QUEEN BEE.png", 4.99);
+INSERT INTO produkt(name, beschreibung, bild_datei, preis)
+VALUES("", "", "SIGN-SHARK.png", 3.99);
+INSERT INTO produkt(name, beschreibung, bild_datei, preis)
+VALUES("", "", "doght indek w.png", 4.99);
+INSERT INTO produkt(name, beschreibung, bild_datei, preis)
+VALUES("", "", "dogmom hund ejje.png", 2.99);
+INSERT INTO produkt(name, beschreibung, bild_datei, preis)
+VALUES("", "", "fische nagelrn.png", 4.99);
+INSERT INTO produkt(name, beschreibung, bild_datei, preis)
+VALUES("", "", "girl who loves horses spruch.png", 3.99);
+INSERT INTO produkt(name, beschreibung, bild_datei, preis)
+VALUES("", "", "hell bee whiser.png", 2.99);
+INSERT INTO produkt(name, beschreibung, bild_datei, preis)
+VALUES("", "", "hell bride to bee 423.png", 3.99);
+INSERT INTO produkt(name, beschreibung, bild_datei, preis)
+VALUES("", "", "helll pferd horse horse.png", 3.99);
+INSERT INTO produkt(name, beschreibung, bild_datei, preis)
+VALUES("", "", "hund spie hallwe.png", 1.99);
+INSERT INTO produkt(name, beschreibung, bild_datei, preis)
+VALUES("", "", "kinde einschuldun.png", 3.99);
+INSERT INTO produkt(name, beschreibung, bild_datei, preis)
+VALUES("", "", "kühehrkphebrk4.png", 3.99);
+INSERT INTO produkt(name, beschreibung, bild_datei, preis)
+VALUES("", "", "kürbis katze3.png", 3.99);
+INSERT INTO produkt(name, beschreibung, bild_datei, preis)
+VALUES("", "", "nope cat.png", 3.99);
+INSERT INTO produkt(name, beschreibung, bild_datei, preis)
+VALUES("", "", "ofen wasser tauch.png", 3.99);
+INSERT INTO produkt(name, beschreibung, bild_datei, preis)
+VALUES("", "", "pferd 6.png", 3.99);
+INSERT INTO produkt(name, beschreibung, bild_datei, preis)
+VALUES("", "", "pferde design 44.png", 3.99);
+INSERT INTO produkt(name, beschreibung, bild_datei, preis)
+VALUES("", "", "pferde jkkliebe.png", 3.99);
+INSERT INTO produkt(name, beschreibung, bild_datei, preis)
+VALUES("", "", "pferde pferd.png", 3.99);
+INSERT INTO produkt(name, beschreibung, bild_datei, preis)
+VALUES("", "", "pferded 42.png", 3.99);
+INSERT INTO produkt(name, beschreibung, bild_datei, preis)
+VALUES("", "", "reiten pferd 84939.png", 3.99);
+INSERT INTO produkt(name, beschreibung, bild_datei, preis)
+VALUES("", "", "save bees s.png", 3.99);
+INSERT INTO produkt(name, beschreibung, bild_datei, preis)
+VALUES("", "", "walking looser pfer d3.png", 3.99);
+
+
+
 
 -- dummy Bestellung
 INSERT INTO bestellung(rechnungsnummer, kunde_id, produkt_id, anzahl) values("R00001", 5, 2, 1);
